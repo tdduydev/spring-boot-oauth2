a2a8b699cba10f59c2586626d72b00b192634c66
hautd4 <hautd4@fpt.com.vn>
2023-04-03 14:13:29 +0700

    structure

---
+++ LoginService/src/test/java/com/fpt/login/LoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
